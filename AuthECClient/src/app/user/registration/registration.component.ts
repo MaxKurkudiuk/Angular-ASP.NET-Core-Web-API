de8c1be2c3dfce3b36bf4870399b99a0d50ddf82
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-05 19:59:50 +0300

    Implement registration form with validation and submission logic

--- AuthECClient/src/app/user/registration/registration.component.ts
+++ AuthECClient/src/app/user/registration/registration.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styles: ``,
 })
-export class Registration {}
+export class Registration {
+    formBuilder = inject(FormBuilder);
+
+    form = this.formBuilder.group({
+        fullName : [''],
+        email : [''],
+        password : [''],
+        confirmPassword : ['']
+    });
+
+    onSubmit(){
+        console.log(this.form.value);
+    }
+}
