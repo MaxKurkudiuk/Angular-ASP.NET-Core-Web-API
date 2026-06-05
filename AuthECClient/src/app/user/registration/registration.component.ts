c1af797a993c978fa0b5a48336905c8ffab6d301
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-05 21:02:46 +0300

    Enhance registration form with validation and error feedback

--- AuthECClient/src/app/user/registration/registration.component.ts
+++ AuthECClient/src/app/user/registration/registration.component.ts
@@ -1,23 +1,47 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registration',
-  imports: [ReactiveFormsModule],
-  templateUrl: './registration.component.html',
-  styles: ``,
+    selector: 'app-registration',
+    imports: [ReactiveFormsModule],
+    templateUrl: './registration.component.html',
+    styles: ``,
 })
 export class Registration {
     formBuilder = inject(FormBuilder);
+    isSubmitted: boolean = false;
+
+    // custom validation
+    passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+        const password = control.get('password')
+        const confirmPassword = control.get('confirmPassword')
+
+        if (password && confirmPassword && password.value != confirmPassword.value)
+            confirmPassword?.setErrors({ passwordMismatch: true })
+        else
+            confirmPassword?.setErrors(null)
+
+        return null;
+    }
 
     form = this.formBuilder.group({
-        fullName : [''],
-        email : [''],
-        password : [''],
-        confirmPassword : ['']
-    });
+        fullName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]],
+        confirmPassword: ['']
+    }, { validators: this.passwordMatchValidator });
 
-    onSubmit(){
+    onSubmit() {
+        this.isSubmitted = true;
         console.log(this.form.value);
+    }
+
+    hasDisplayableError(controlName: string): Boolean {
+        const control = this.form.get(controlName);
+        return Boolean(control?.invalid) &&
+            (this.isSubmitted || Boolean(control?.touched))
     }
 }
