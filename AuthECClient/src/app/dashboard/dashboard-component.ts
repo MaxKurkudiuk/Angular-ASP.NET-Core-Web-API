e2c883d3ca7649f7e30061b1f86f14f453ef926c
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-07 15:57:13 +0300

    Add dashboard component

---
+++ AuthECClient/src/app/dashboard/dashboard-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard-component.html',
+  styles: ``,
+})
+export class DashboardComponent {}
