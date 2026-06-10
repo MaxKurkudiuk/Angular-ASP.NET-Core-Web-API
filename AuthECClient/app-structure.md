2fd72b72bb7962348f188d459b1d2c4cc5994d60
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-10 11:25:39 +0300

    Add and use environment configuration

--- AuthECClient/app-structure.md
+++ AuthECClient/app-structure.md
@@ -1,16 +1,20 @@
 src
-    app
-        user
-            user.component.ts|.html
-            login (cmp. folder)
-            registration (cmp. folder)
+  app
+    user
+      user.component.ts|.html
+      login (cmp. folder)
+      registration (cmp. folder)
 
-        dashboard (cmp. foledr)
-        
-    shared
-        auth.guard.ts
-        constants.ts
-        pipes
-            first-key.pipe.ts
-        services
-            auch.service.ts
+    dashboard (cmp. foledr)
+    environments
+      environment.ts
+      environment.development.ts
+      
+  shared
+    auth.guard.ts
+    constants.ts
+    pipes
+      first-key.pipe.ts
+    services
+      user.service.ts
+      auch.service.ts
--- AuthECClient/src/app/shared/services/auth.service.ts
+++ AuthECClient/src/app/shared/services/auth.service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { TOKEN_KEY } from '../constants';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
     private http = inject(HttpClient);
-    baseUrl = 'http://localhost:5292/api';
 
     createUser(formData:any){
-        return this.http.post(this.baseUrl + '/signup', formData);
+        return this.http.post(environment.apiBaseUrl + '/signup', formData);
     }
 
     signin(formData:any){
-        return this.http.post(this.baseUrl + '/signin', formData);
+        return this.http.post(environment.apiBaseUrl + '/signin', formData);
     }
 
     isLoggedIn(){
