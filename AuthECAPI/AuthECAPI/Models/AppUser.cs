4394698d8e7cca33823851f141955a35f09b3e2a
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-10 17:05:15 +0300

    Add properties for DOB, Gender, and LibraryID to AppUser model and update migrations

--- AuthECAPI/AuthECAPI/Models/AppUser.cs
+++ AuthECAPI/AuthECAPI/Models/AppUser.cs
@@ -8,4 +8,14 @@ public class AppUser : IdentityUser
     [PersonalData]
     [Column(TypeName ="nvarchar(150)")]
     public string FullName { get; set; } = null!;
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(10)")]
+    public string Gender { get; set; } = null!;
+
+    [PersonalData]
+    public DateOnly DOB { get; set; }   // Day of birth
+
+    [PersonalData]
+    public int? LibraryID { get; set; }
 }
