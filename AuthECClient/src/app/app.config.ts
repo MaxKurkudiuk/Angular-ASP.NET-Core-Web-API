b6b2c8298a2d4af2f17b2d5e5ef8be7d7af3cc8c
Maksym Kurkudiuk <max.kurkudiuk@gmail.com>
2026-06-14 12:59:08 +0300

    Fix import path for authInterceptor and add interceptor implementation

--- AuthECClient/src/app/app.config.ts
+++ AuthECClient/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/interceptors/auth-interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
